58a64a97667db4aa444555761e356bc6466923b8
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-07 17:35:16 +0900

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,8 @@
+ $(document).ready(function () {
+        var vds = document.getElementById('vds');
+		if (vds) {
+        vds.src = "https://vod.plaync.com/JM/preorder/240930/bg_01.mp4#t=0.1";
+        document.getElementById('vd').load();
+		}
+    });
+
